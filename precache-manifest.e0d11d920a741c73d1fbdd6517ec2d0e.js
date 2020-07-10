94d59d3cafb99f2292dab4ac3d6e7023681dad76
Flan Mcgan <flanmcgan@protonmail.com>
2020-07-10 09:11:32 -0400

    Updates

---
+++ precache-manifest.e0d11d920a741c73d1fbdd6517ec2d0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "095f160c7e98d321f3b6d41f939de9d6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ec6f8b64c40f88ce8b68",
+    "url": "/monsters-rolodex/static/css/main.972a7b9e.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec6f8b64c40f88ce8b68",
+    "url": "/monsters-rolodex/static/js/main.66be8ed4.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
